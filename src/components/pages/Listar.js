fbb68310c8e40f92d1dad46f4331319285d780fb
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-03-09 21:42:16 -0300

    chore: Change in the button of the List component.

--- src/components/pages/Listar.js
+++ src/components/pages/Listar.js
@@ -5,20 +5,16 @@ import { Link } from "react-router-dom";
 import styles from "./Listar.module.css";
 
 function Listar() {
-  const [nameSeach, setNameSeach] = useState("");
- const [showListRegister, setShowListRegister] = useState(true);
+  const [nameSearch, setNameSearch] = useState("");
+  const [showListRegister, setShowListRegister] = useState(true);
+  const [showSearch, setShowSearch] = useState(null);
 
-
- //Testando!!
- const hide = () => setShowListRegister(false)
- const show = () => setShowListRegister(true)
-  //const [showPatientRegister, setShowPatientRegister] = useState(false)
-
-  
+  const hide = () => setShowListRegister(false);
+  const show = () => setShowListRegister(true);
 
   const listaDePacientes = JSON.parse(localStorage.getItem("dadosPac")) || [];
-  
-  function seach(event) {
+
+  function search(event) {
     event.preventDefault();
 
     //resgata dados do localstorage e armazena com nome "dadosPac"
@@ -32,19 +28,20 @@ function Listar() {
     //console.log('novaLista', novaLista)
 
     let valid = false;
-    while (nameSeach.length === 0)
-      nameSeach = alert("Precisa digitar um nome!");
+    while (nameSearch.length === 0)
+      nameSearch = alert("Precisa digitar um nome!");
     while (valid !== true) {
-      let confirm = `O nome "${nameSeach}" não está na lista de cadastra!`;
+      let confirm = `O nome "${nameSearch}" não está na lista de cadastra!`;
       for (let i of novaLista) {
-        if (i.name === nameSeach) {
+        if (i.name === nameSearch) {
           confirm = `"${i.name}" esta cadastrado`;
           let cadastroPaciente = JSON.stringify(i);
-          //setShowListRegister(false)
-          document.getElementById("listaCadastro").style.display = "none";
+
+          setShowListRegister(false);
+          setShowSearch(true);
+
+          //Preciso eliminar esse 'getElementById()"
           document.getElementById("patientName").innerHTML = cadastroPaciente;
-          //setShowPatientRegister(true)
-          document.getElementById("toSeach").style.display = "block";
         }
       }
       alert(confirm);
@@ -67,12 +64,12 @@ function Listar() {
     //console.log('novaLista', novaLista)
 
     let valid = false;
-    while (nameSeach.length === 0)
-      nameSeach = alert("Precisa digitar um nome!");
+    while (nameSearch.length === 0)
+      nameSearch = alert("Precisa digitar um nome!");
     while (valid !== true) {
-      let returno = `O nome "${nameSeach}" não está cadastrado!`;
+      let returno = `O nome "${nameSearch}" não está cadastrado!`;
       for (let i of novaLista) {
-        if (i.name === nameSeach) {
+        if (i.name === nameSearch) {
           let confirm = window.confirm(
             `Confirma a remoção de "${i.name}" da lista de cadastrado?`
           );
@@ -80,7 +77,7 @@ function Listar() {
             let item = JSON.parse(localStorage.getItem("dadosPac")); //Lista transformada em objeto
 
             var myArray = item;
-            var newArray = myArray.filter((item) => item.name !== nameSeach);
+            var newArray = myArray.filter((item) => item.name !== nameSearch);
             //alert(JSON.stringify(newArray)); //lista atualizada sem o nome desejada
 
             //criar lista no localstorage com nome "dadosPac" com os valores da variável "newArray"
@@ -105,58 +102,56 @@ function Listar() {
             <input
               type="search"
               placeholder="Digite o nome..."
-              value={nameSeach}
-              onChange={(e) => setNameSeach(e.target.value)}
+              value={nameSearch}
+              onChange={(e) => setNameSearch(e.target.value)}
             />
           </label>
-          <button onClick={seach}>Pequisar</button>
+          <button onClick={search}>Pequisar</button>
           <button onClick={remove}>Remover</button>
         </div>
-        {showListRegister ? 
-        <div id="listaCadastro">
-        <hr></hr>
-          <table>
-            <thead>
-              <tr>
-                <th>CPF</th>
-                <th>Nome</th>
-                <th>Endereço</th>
-                <th>Data nasc</th>
-                <th>Sexo</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {listaDePacientes.map((paciente) => (
+        {showSearch ? ( //Se confirma null
+          <div className={styles.toSearch} id="toSearch">
+            <hr></hr>
+            <h2>Paciente pesquisado</h2>
+            <div id="patientName"></div>
+            <hr></hr>
+            <div>
+              <button onClick={show}>Mostrar Lista Cadastro</button>
+              <button onClick={hide}>Esconder Lista Cadastro</button>
+            </div>
+          </div>
+        ) : null}
+        {showListRegister ? ( //Se confirma null
+          <div id="listaCadastro">
+            <hr></hr>
+            <h2>Lista de pacientes cadastrados</h2>
+            <table>
+              <thead>
                 <tr>
-                  <td>{paciente.cpf}</td>
-                  <td>{paciente.name}</td>
-                  <td>{paciente.address}</td>
-                  <td>{paciente.birthdate}</td>
-                  <td>{paciente.gender}</td>
-                  <td>{paciente.status}</td>
+                  <th>CPF</th>
+                  <th>Nome</th>
+                  <th>Endereço</th>
+                  <th>Data nasc</th>
+                  <th>Sexo</th>
+                  <th>Status</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <hr></hr>
-        </div>
-        : null }
-      </div>
-      <div>
-        
-      <button onClick={hide}>testeEscoderLista</button>
-      <br></br>
-      <button onClick={show}>testeMostrarLista</button>
-      <br></br>
-      </div>
-      <div className={styles.toSeach} id="toSeach">
-        <h2>Paciente pesquisado</h2>
-        <hr></hr>
-        <br></br>
-        <div id="patientName">
-        </div>
-        <hr></hr>
+              </thead>
+              <tbody>
+                {listaDePacientes.map((paciente) => (
+                  <tr>
+                    <td>{paciente.cpf}</td>
+                    <td>{paciente.name}</td>
+                    <td>{paciente.address}</td>
+                    <td>{paciente.birthdate}</td>
+                    <td>{paciente.gender}</td>
+                    <td>{paciente.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <hr></hr>
+          </div>
+        ) : null}
       </div>
       <div>
         <Link to="/">Voltar</Link>
